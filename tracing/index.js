b70698f1101feaaa9bea22be5bbc9eab2d2a1aea
Yannick Reifschneider <yannick.reifschneider@stud.tu-darmstadt.de>
2019-08-01 22:54:48 +0200

    Process trace

--- tracing/index.js
+++ tracing/index.js
@@ -1,6 +1,13 @@
 const PARALLEL_BROWSERS = 5;
-const ANALYSE_TIME_SECS = 60;
+const ANALYSE_TIME_SECS = 15;
 const PAGE_LOAD_TIMEOUT_SECS = 120;
+
+const TRACE_EVENT_WINDOW_POSTMESSAGE = 'trace_event_window_postmessage';
+const TRACE_EVENT_WORKER_CREATED = 'trace_event_worker_created';
+const TRACE_EVENT_WORKER_POSTMESSAGE = 'trace_event_worker_postmessage';
+const TRACE_EVENT_WEBSOCKET_CREATED = 'trace_event_websocket_created';
+const TRACE_EVENT_WEBSOCKET_CONNECTED = 'trace_event_websocket_connected';
+const TRACE_EVENT_WEBSOCKET_DISCONNECTED = 'trace_event_websocket_disconnected';
 
 const puppeteer = require("puppeteer");
 const fs = require("fs");
@@ -9,14 +16,27 @@ const parse = require("csv-parse");
 const transform = require('stream-transform');
 const stringify = require('csv-stringify');
 const unlinkAsync = promisify(fs.unlink);
+const { loadDevtoolsModel, calculateScriptingTimeFraction } = require('./process-trace')
+
+const args = process.argv.slice(2);
+const sitesFile = args.length >= 1 ? args[0] : 'top-1m.csv';
 
 const hookPostMessage = () => {
   const originalPostMessage = postMessage;
   window.postMessage = function() {
-    console.log("postMessage called", ...arguments);
     reportPostMessage(...arguments);
+    console.count("postMessage");
+    console.timeStamp("postMessage");
     return originalPostMessage(...arguments);
   };
+};
+
+const createTracingWorker = WorkerImpl => class extends WorkerImpl {
+  constructor(...args) {
+    super(...args);
+    reportWorker(...args);
+    console.timeStamp("Worker created")
+  }
 };
 
 const hookWorker = () => {
@@ -34,8 +54,8 @@ const hookWorker = () => {
 
 const normlizeUrl = url =>
   url
-    .replace('.', '_')
-    .replace('/', '__');
+    .replace(/\./g, '_')
+    .replace(/\//g, '__');
 
 const tracer = transform(async ([siteNo, siteUrl], done) => {
   let browser;
@@ -62,31 +82,69 @@ const tracer = transform(async ([siteNo, siteUrl], done) => {
     await page.goto(`http://${siteUrl}`, { timeout: PAGE_LOAD_TIMEOUT_SECS * 1000 });
     await page.waitFor(1000);
     const blankPage = await browser.newPage();
-    const outFilePath = `out/${siteNo}_${normlizeUrl(siteUrl)}.json`;
-    await page.tracing.start({ path: outFilePath });
+    const traceFilePath = `out/${siteNo}_${normlizeUrl(siteUrl)}.json`;
+    await page.tracing.start({ path: traceFilePath });
     await page.waitFor(ANALYSE_TIME_SECS * 1000);
     await page.tracing.stop();
     const pptrWorkers = page.workers().map(worker => worker.url());
-    if (hookedWorkers.length === 0 && pptrWorkers.length === 0 && postMessage.length === 0) {
-      await unlinkAsync(outFilePath)
+    const backgroundExecutionDetected = !(hookedWorkers.length === 0 && pptrWorkers.length === 0 && postMessage.length === 0)
+    if (!backgroundExecutionDetected) {
+      await unlinkAsync(traceFilePath)
     }
-    const result = { hookedWorkers, pptrWorkers, postMessage };
-    done(null, [siteNo, siteUrl, JSON.stringify((result))])
+    done(null, {
+      siteNo,
+      siteUrl,
+      traceFilePath,
+      backgroundExecutionDetected,
+      hookedWorkers, pptrWorkers, postMessage
+    });
   } catch (e) {
-    const result = { error: e.message };
-    done(null, [siteNo, siteUrl, JSON.stringify(result)]);
+    done(null, {
+      siteNo,
+      siteUrl,
+      error: e
+    });
   } finally {
     await browser.close()
   }
 }, { parallel: PARALLEL_BROWSERS });
 
+const processTrace = transform(async (obj, done) => {
+  console.log('processTrace', obj);
+  if (obj.error || !obj.backgroundExecutionDetected) {
+    done(null, obj)
+  } else {
+    try {
+      const model = await loadDevtoolsModel(obj.traceFilePath);
+      const scriptingTimeFraction = calculateScriptingTimeFraction(model);
+      done(null, {
+        ...obj,
+        scriptingTimeFraction,
+      })
+    } catch (e) {
+      done(e)
+    }
+  }
+});
+
+const transformToOutputCSV = transform((obj, done) => {
+  const { siteNo, siteUrl, error, scriptingTimeFraction, traceFilePath, hookedWorkers, pptrWorkers, postMessage } = obj;
+  done(null, [
+    siteNo,
+    siteUrl,
+    error ? error.message : '-',
+    scriptingTimeFraction || NaN,
+    traceFilePath,
+    JSON.stringify({ hookedWorkers, pptrWorkers, postMessage })
+  ])
+});
 
 (async () => {
-  const inputPath = "top-1m.csv";
-  //const inputPath = "test.csv";
-  fs.createReadStream(inputPath)
+  fs.createReadStream(sitesFile)
     .pipe(parse())
     .pipe(tracer)
+    .pipe(processTrace)
+    .pipe(transformToOutputCSV)
     .pipe(stringify())
     .pipe(fs.createWriteStream("out/output.csv"));
 })();
---
+++ tracing/process-trace.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const { promisify } = require('util');
+const readFileAsync = promisify(fs.readFile);
+const DevtoolsTimelineModel = require('devtools-timeline-model');
+
+const loadDevtoolsModel = async (traceFilePath) => {
+  const traceEvents = await readFileAsync(traceFilePath, 'utf8');
+  return new DevtoolsTimelineModel(traceEvents);
+};
+
+const calculateScriptingTimeFraction = (model) => {
+  const tracingModel = model.tracingModel();
+  const eventCategories = model.bottomUpGroupBy('Category');
+  const recordingTime = tracingModel.maximumRecordTime() - tracingModel.minimumRecordTime();
+  const scriptingTime = eventCategories.children.get('scripting').totalTime;
+  return scriptingTime / recordingTime;
+};
+
+module.exports = {
+  loadDevtoolsModel,
+  calculateScriptingTimeFraction
+};
