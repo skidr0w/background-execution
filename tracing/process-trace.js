1de34c9a4e5795eb085338e4905eadc415d14712
Yannick Reifschneider <yannick.reifschneider@stud.tu-darmstadt.de>
2019-08-06 20:48:34 +0200

    progress and try catch

--- tracing/process-trace.js
+++ tracing/process-trace.js
@@ -6,19 +6,34 @@ const readdirAsync = promisify(fs.readdir);
 const writeFileAsync = promisify(fs.writeFile);
 const stringify = require('csv-stringify');
 const stringifyAsync = promisify(stringify);
+const readline = require('readline');
 
 const OUT_DIR = './out';
 const RESULTS_FILE = 'sorted.csv';
+
+const reportProgress = (current, total) => {
+  process.stderr.write('\x1B7');
+  readline.clearLine(process.stderr);
+  process.stderr.write(`Completed ${current} of ${total}, ${Math.round(current / total * 100)}%`);
+  process.stderr.write('\x1B8');
+};
 
 (async () => {
   const files = await readdirAsync(OUT_DIR);
   const traceFiles = files.filter(file => file.endsWith('.json'));
   const results = [];
-  for (const traceFile of traceFiles) {
-    const filePath = path.join(OUT_DIR, traceFile);
-    const model = await loadDevtoolsModel(filePath);
-    const scriptingTimeFraction = calculateScriptingTimeFraction(model);
-    results.push([traceFile, scriptingTimeFraction]);
+  const total = traceFiles.length;
+  for (let i = 0; i < total; i++) {
+    try {
+      const traceFile = traceFiles[i];
+      const filePath = path.join(OUT_DIR, traceFile);
+      const model = await loadDevtoolsModel(filePath);
+      const scriptingTimeFraction = calculateScriptingTimeFraction(model);
+      results.push([traceFile, scriptingTimeFraction]);
+    } catch (e) {
+      console.log('Could not process file: %s (%s)', traceFile, e.message);
+    }
+    reportProgress(i + 1, total);
   }
   const sortedResults = results.sort((a, b) => a[1] > b[1] ? -1 : 1);
   const csv = await stringifyAsync(sortedResults);
