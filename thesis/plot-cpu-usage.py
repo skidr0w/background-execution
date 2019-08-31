0446a7b1e5b254d890653d4bce89e01f051cd5d5
Yannick Reifschneider <yannick.reifschneider@stud.tu-darmstadt.de>
2019-08-31 16:48:53 +0200

    Include graphs in thesis

--- thesis/plot-cpu-usage.py
+++ thesis/plot-cpu-usage.py
@@ -25,6 +25,7 @@ with open(infile, 'r') as f:
         x.append(float(row[0]) / 1000)
         y.append(float(row[1]) * 100)
 
+f = plt.figure()
 ax = plt.axes()
 
 plt.step(x, y)
@@ -35,4 +36,6 @@ plt.xlabel('Time in background [mm:ss]')
 plt.ylabel('CPU usage')
 top, bottom = plt.ylim()
 plt.ylim(bottom = min(bottom, 0), top = max(top, 100))
+f.savefig(infile + '.pgf')
+f.savefig(infile + '.pdf')
 plt.show()
