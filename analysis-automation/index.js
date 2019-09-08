33b5bab2ff15d68c7ba4513348846d18c4a93076
Yannick Reifschneider <yannick.reifschneider@stud.tu-darmstadt.de>
2019-09-08 13:22:21 +0200

    More robust automation

--- analysis-automation/index.js
+++ analysis-automation/index.js
@@ -8,11 +8,21 @@ const firefoxOptions = new firefox.Options().setPreference(
   'application/octet-stream',
 );
 
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 const measure = async (url, browser, workTime) => {
-  let driver = await new Builder()
-    .forBrowser(browser)
-    .setFirefoxOptions(firefoxOptions)
-    .build();
+  let driver;
+  do {
+    try {
+      driver = await new Builder()
+        .forBrowser(browser)
+        .setFirefoxOptions(firefoxOptions)
+        .build();
+    } catch (err) {
+      console.log('Could not open driver session', err.message);
+      await sleep(5000);
+    }
+  } while (driver === undefined);
   try {
     await driver.get(url);
     const workDurationInput = driver.findElement(By.css('#work-duration'));
@@ -20,21 +30,24 @@ const measure = async (url, browser, workTime) => {
     await workDurationInput.sendKeys(workTime);
     await driver.sleep(1500);
     await driver.findElement(By.css('#start')).click();
-    await driver.sleep(MEASURE_TIME_SEC * 1000);
+    await driver.sleep(500);
     const windowHandles = await driver.getAllWindowHandles();
+    await driver.sleep(MEASURE_TIME_SEC * 1000);
     await driver.switchTo().window(windowHandles[1]);
     await driver.close();
     await driver.switchTo().window(windowHandles[0]);
-    await driver.sleep(1000);
+    await driver.sleep(5000);
     await driver.findElement(By.css('#download')).click();
-    await driver.sleep(1000);
+    await driver.sleep(2500);
     console.log(
-      `Finished measurement for ${browser} and ${workTime}ms work time`,
+      `Finished measurement for ${browser} and ${url} and ${workTime}ms work time`,
     );
-    await driver.quit();
   } catch (err) {
     console.error('Error:', err.message);
+  } finally {
+    await driver.quit();
   }
+  await sleep(2500);
 };
 
 const urls = [
@@ -47,8 +60,8 @@ const browsers = ['chrome', 'firefox', 'safari'];
 const workTimes = ['50', '1000'];
 
 (async () => {
-  for (const url of urls) {
-    for (const browser of browsers) {
+  for (const browser of browsers) {
+    for (const url of urls) {
       for (const workTime of workTimes) {
         await measure(url, browser, workTime);
       }
